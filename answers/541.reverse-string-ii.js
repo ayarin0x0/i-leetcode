fc427cf6dc9c6aeac8ffc8711d9ee29feccf91b2
ayarin0x0 <me@ayarin.moe>
2024-10-24 04:00:14 +0800

    ٩(ˊᗜˋ*)

--- answers/541.reverse-string-ii.js
+++ answers/541.reverse-string-ii.js
@@ -15,28 +15,28 @@
  *
  * Given a string s and an integer k, reverse the first k characters for every
  * 2k characters counting from the start of the string.
- * 
+ *
  * If there are fewer than k characters left, reverse all of them. If there are
  * less than 2k but greater than or equal to k characters, then reverse the
  * first k characters and leave the other as original.
- * 
- * 
+ *
+ *
  * Example 1:
  * Input: s = "abcdefg", k = 2
  * Output: "bacdfeg"
  * Example 2:
  * Input: s = "abcd", k = 2
  * Output: "bacd"
- * 
- * 
+ *
+ *
  * Constraints:
- * 
- * 
+ *
+ *
  * 1 <= s.length <= 10^4
  * s consists of only lowercase English letters.
  * 1 <= k <= 10^4
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
@@ -46,7 +46,47 @@
  * @return {string}
  */
 var reverseStr = function(s, k) {
-    
+  let result = ''
+
+  const interval = 2 * k
+  const leftStringLen = s.length % interval
+  const times = Math.ceil(s.length / interval)
+  const reverseTimes = leftStringLen === 0 ? times : times - 1
+
+  for (let time = 0; time < reverseTimes; time++) {
+    const base = interval * time
+    for (let i = k - 1; i >= 0; i--) {
+      result += s[base + i]
+    }
+
+    const leftStringStart = base + k
+    for (let i = 0; i < k; i++) {
+      result += s[leftStringStart + i]
+    }
+  }
+
+  // handle last loop
+  if (leftStringLen > 0) {
+    const base = interval * reverseTimes
+    const start = Math.min(leftStringLen, k)
+    for (let i = start - 1; i >= 0; i--) {
+      result += s[base + i]
+    }
+
+    if (leftStringLen > k) {
+      const leftStringStart = base + k
+      const end = leftStringLen - k
+      for (let i = 0; i < end; i++) {
+        result += s[leftStringStart + i]
+      }
+    }
+  }
+
+  return result
 };
 // @lc code=end
 
+console.log(
+  'result',
+  reverseStr("abcdefg", 2)
+)
